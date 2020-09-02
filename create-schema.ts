e1aef02bdd5a762e77355ff381864b4bc7363682
Jeff Detmer <jeff.detmer@gmail.com>
2020-09-02 02:59:38 -0400

    fix(create-schema): update the imports and ignore during build

--- create-schema.ts
+++ create-schema.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from '@mikro-orm/core'
-import config from './mikro-orm.config'
+import config from './src/mikro-orm.config'
 ;(async () => {
   const orm = await MikroORM.init(config)
   const generator = orm.getSchemaGenerator()
